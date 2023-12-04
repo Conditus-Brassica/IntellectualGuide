98250256b8701447ee405bb5d4cdf5d202f2db68
Vasilich563 <yackub563@mail.ru>
2023-12-04 14:22:17 +0300

    update crud_agent.py
    remove test from crud_agent.py

--- backend/agents/crud_agent/crud_agent.py
+++ backend/agents/crud_agent/crud_agent.py
@@ -150,37 +150,3 @@ class CRUDAgent(PureCRUDAgent):
             await logger.info(f"get_recommendations_for_landmark_by_region. "
                               f"Validation error on json, args: {ex.args[0]}, json_params: {json_params}")
             raise ValidationError
-
-
-if __name__ == '__main__':
-    from pprint import pprint
-    from backend.agents.crud_agent.reader import Reader
-    from neo4j import AsyncGraphDatabase
-
-    async def test():
-
-        async with AsyncGraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'ostisGovno')) as driver:
-            reader = Reader()
-            crud = CRUDAgent(reader, driver, 'neo4j')
-
-            task1 = asyncio.create_task(crud.get_categories_of_region({"region_name": "Мядзельскі раён"}))
-            task2 = asyncio.create_task(crud.get_landmarks_in_map_sectors({"sector_names": ["a1", "a2"], "optional_limit": 3}))
-            task3 = asyncio.create_task(crud.get_landmarks_refers_to_categories({"categories_names": ["озёра мядельского района", "национальные парки белоруссии"], "optional_limit": 3}))
-            task4 = asyncio.create_task(crud.get_landmarks_by_coordinates({"coordinates": [{"longitude": 26.91887917, "latitude": 54.84001}, {"longitude": 26.8629, "latitude":54.955}, {"longitude":26.8684 , "latitude":54.9683}], "optional_limit": 3}))
-            task5 = asyncio.create_task(crud.get_landmarks_by_names({"landmark_names": ["свирь", "рудаково", "нарочь"], "optional_limit": 3}))
-            task6 = asyncio.create_task(crud.get_landmarks_of_categories_in_region({"region_name": "Мядзельскі раён", "categories_names": ["национальные парки белоруссии"]}))
-            task7 = asyncio.create_task(crud.get_landmarks_by_region({"region_name": "Мядзел", "optional_limit": 3}))
-            task8 = asyncio.create_task(crud.get_recommendations_for_landmark_by_region(
-                {"user_login": "user", "current_latitude": 54.8964, "current_longitude": 26.8922, "current_name": "рудаково (озеро)", "amount_of_recommendations": 10}
-            ))
-
-            pprint(await task1)
-            pprint(await task2)
-            pprint(await task3)
-            pprint(await task4)
-            pprint(await task5)
-            pprint(await task6)
-            pprint(await task7)
-            pprint(await task8)
-
-    asyncio.run(test())
