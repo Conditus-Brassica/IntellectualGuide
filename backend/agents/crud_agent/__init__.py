64d6e00d65cebf95ca8b850b6252cd8e05b6cb75
Vasilich563 <yackub563@mail.ru>
2023-12-04 14:33:26 +0300

    update __init__.py of crud_agent module

--- backend/agents/crud_agent/__init__.py
+++ backend/agents/crud_agent/__init__.py
@@ -3,4 +3,7 @@
 Crud agent module
 https://sefon.pro/mp3/474614-sektor-gaza-narkoman/
 """
+
+from .crud_commands import *
 from .crud_agent import CRUDAgent
+from .pure_classes import PureCRUDAgent
