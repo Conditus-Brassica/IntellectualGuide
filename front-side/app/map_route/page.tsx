cead5a684af4b3f44158558f70287ff5605b22ad
lapuskin <mikhail.lapkovskiy.2004@gmail.com>
2023-12-04 16:49:07 +0300

    add card

--- front-side/app/map_route/page.tsx
+++ front-side/app/map_route/page.tsx
@@ -1,17 +1,26 @@
 'use client'
-
+import CustomMarker from "@/components/TheMap";
 import Head from "next/head";
 import styles from "@/styles/main_page.module.css";
 import Map from "@/components/TheMap";
-
 import TheSidePanel from "@/components/TheSidePanel";
 import { useState } from "react";
-
+import TheLandmarkCard from "@/components/TheLandmarkCard";
 
 export default function MapRoute() {
+
   const [mapData, setMapData] = useState({
     markers: {}
   });
+
+  const [landmark, setLandmark] = useState(false);
+
+
+  const [markerState, setMarkerState] = useState({
+    targetMarker: CustomMarker
+  })
+
+
 
   return (
     <>
@@ -23,7 +32,8 @@ export default function MapRoute() {
       <main className={styles.main_page}>
         <div>
           <TheSidePanel mapData={mapData} setMapData={setMapData}/>
-          <Map mapData={mapData} setMapData={setMapData}/> 
+          <TheLandmarkCard markerState={markerState} landmark={landmark}/>
+          <Map setMapData={setMapData} markerState={markerState} setMarkerState={setMarkerState} setLandmark={setLandmark}/> 
         </div>
       </main>
     </>
--- front-side/components/TheLandmarkCard.tsx
+++ front-side/components/TheLandmarkCard.tsx
@@ -1,18 +1,43 @@
 import style from "@/styles/landmark_card.module.css"
+import sidepanel_style from "@/styles/sidepanel.module.css"
+import TheButton from "./TheButton";
+interface TheLandmarkCard {
+    markerState: any;
+    landmark: any;
+}
 
-const TheLandmarkCard = () =>{
-    return(
-    <div>
-        <div className={style.image}>
-            <img src="" alt="" />
-        </div>
-        <div className={style.name}>
-            <h1></h1>
-        </div>
-        <div className={style.description}>
-            <p></p>
-        </div>
-    </div>
+const TheLandmarkCard: React.FC<TheLandmarkCard> = ({ markerState, landmark }) => {
+    async function getPointInfo() {
+        try {
+          const response = await fetch('https://example.com/data');
+          const data = await response.json();
+          console.log(data);
+          return data
+        } catch (error) {
+          alert("Что-то пошло не так! Проверьте соединение с интернетом!");
+        }
+      };
+   
+    const buildARoute = function () {
+
+    }
+    return (<>
+        {<div style={{ display: landmark ? 'block' : 'none' }}>
+                <div className={sidepanel_style.panel}>
+                    <div className={style.image}>
+                        <img src="" alt="" />
+                    </div>
+                    <div className={style.name}>
+                        <h3></h3>
+                    </div>
+                    <div className={style.description}>
+                        <p></p>
+                    </div>
+                    <TheButton title="Построить маршрут!" func={buildARoute} />
+                </div>
+            </div>
+        }
+    </>
     )
 }
 
