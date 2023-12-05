6a0404bb85070d2dd5329a7601666fbbcdd0061f
stas <stanislav.voitkus@outlook.com>
2023-12-05 15:50:49 +0300

    routing agent docstr

--- backend/agents/routing_agent/route_generating.py
+++ backend/agents/routing_agent/route_generating.py
@@ -4,11 +4,21 @@ import api_key
 
 
 class RoutingAgent:
+    """
+    Agent for finding route points.
+    Usage: when you need to create optimized
+    route between several points.
+    """
     def __init__(self):
         self.__client__ = ors.Client(key=api_key.__key__)
         self.__landmarks__ = []
 
     def set_points(self, points_list: list):
+        """
+        Method finds all route points for provided points.
+        :param points_list: [[latitude: float, longitude: float], ...]
+        :return:
+        """
         self.__landmarks__ = points_list
 
         self.__landmarks__ = self.__reverse_coordinates__(self.__landmarks__)
@@ -21,7 +31,7 @@ class RoutingAgent:
     def __reverse_coordinates__(coordinates_list: list):
         """
         Method revere coordinates because of osm (longitude, latitude),
-        But normally it should be (latitude, longitude)
+        but normally it should be (latitude, longitude).
         :param coordinates_list:
         :return: list of reversed coordinates
         """
@@ -30,6 +40,10 @@ class RoutingAgent:
         return coordinates_list
 
     def __create_optimized_route__(self):
+        """
+        Interaction with OpenRoutService API
+        :return:
+        """
         route = self.__client__.directions(
             coordinates=self.__landmarks__,
             profile='driving-car',
