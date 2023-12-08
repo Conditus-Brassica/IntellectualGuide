bb9a8ce42f227e09eff48ec155c0d623b080cc6c
Meteorych <super.titlov@inbox.ru>
2023-12-08 23:38:03 +0300

    dockstring?

--- backend/agents/get_landmarks_in_sector/get_landmarks_in_sectors_commands/get_landmarks_by_categories_in_sector.py
+++ backend/agents/get_landmarks_in_sector/get_landmarks_in_sectors_commands/get_landmarks_by_categories_in_sector.py
@@ -11,24 +11,31 @@ class LandmarksOfCategoriesInUserViewCommand(BaseCommand):
     Command to get landmarks that located in given map sectors and refer to given categories by coordinates of user view's borders.
     """
 
-    def __init__(self, get_landmarks_in_sectors: GetLandmarksInSectorsAgent, coords_of_square: Dict, agent: PureCRUDAgent):
+    def __init__(self, get_landmarks_in_sectors: GetLandmarksInSectorsAgent, coords_of_square: Dict, categories: dict,
+                 crud_agent: PureCRUDAgent):
         """
         Creates command to get landmarks that located in given map sectors and refer to given categories from
         getLandmarksInSectorsAgent.
 
-        :param get_landmarks_in_sectors_agent: getLandmarksInSectorsAgent child class entity to get from
+        :param get_landmarks_in_sectors: getLandmarksInSectorsAgent child class entity to get from
         :param coords_of_square: Dict in form {
          "TL": {
-             "latitude": double,
-             "longitude": double
+             "latitude": Double,
+             "longitude": Double
          },
          "BR": {
-             "latitude": double,
-             "longitude": double
+             "latitude": Double,
+             "longitude": Double
          }
        }
+        :param categories: Dict in form {
+            "categories": List
+        }
+        :param crud_agent: PureCRUDAgent child class entity to get from
         """
-        self._json_params = json_params
+        self._json_params1 = coords_of_square
+        self._json_params2 = categories
+        self.agent = crud_agent
         super().__init__(get_landmarks_in_sectors)
 
     async def execute(self):
@@ -45,4 +52,4 @@ class LandmarksOfCategoriesInUserViewCommand(BaseCommand):
                 ]
             ]
         """
-        return await self._target_agent.get_landmarks_of_categories_in_map_sectors(self._json_params)
+        return await self._target_agent.get_landmarks_of_categories_in_map_sectors(self._json_params1, self._json_params2, self.agent)
