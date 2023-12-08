5539823106cc6f5e9d6285b6c9977d18eb6e25cc
stas <stanislav.voitkus@outlook.com>
2023-12-08 21:36:14 +0300

    requirements.txt update

--- backend/broker/agents_broker.py
+++
@@ -1,9 +0,0 @@
-from taskiq_redis import ListQueueBroker, RedisAsyncResultBackend
-
-from pure_broker import PureBroker
-
-
-class AgentsBroker(PureBroker):
-    broker = ListQueueBroker(
-        url="redis://localhost:6379"
-    ).with_result_backend(RedisAsyncResultBackend(redis_url="redis://localhost:6379"))
