764adb60ba848088b50cfbc171581b364ac20c2d
lapuskin <mikhail.lapkovskiy.2004@gmail.com>
2023-12-11 18:09:12 +0300

    update styles

--- front-side/components/TheRoutePanel.tsx
+++ front-side/components/TheRoutePanel.tsx
@@ -2,6 +2,7 @@ import sidepanel_style from "@/styles/sidepanel.module.css"
 import TheButton from "./TheButton";
 import { useEffect } from "react";
 import CustomMarker from "./TheMap"
+import style from "@/styles/route_panel.module.css"
 
 interface TheRoutePanel {
     setRoute: any;
@@ -23,15 +24,19 @@ const TheRoutePanel: React.FC<TheRoutePanel> = ({ setRoute, route, mapData }) =>
         <>
             {<div style={{ display: route ? 'block' : 'none' }}>
                 <div className={sidepanel_style.panel}>
-                    {/* <div className={style.name}>
-                        */}<h3>Маршрут</h3>{/*
+                    <div className={style.name}>
+                       <h3>Маршрут</h3>
                     </div>
-                    <div className={style.description}>
-                        <p></p>
-                    </div> */}
-                    {mapData.markers.map((value: typeof CustomMarker, index:number) => (
-                        <p key={index}>{value.name}</p>
+                   <div>
+                   {mapData.markers.map((value: typeof CustomMarker, index:number) => (
+                        <div className={style.row}>
+                            <div className={style.number}>
+                                <span>{index + 1}</span>
+                            </div>
+                            <p key={index +1}>{value.name}</p>
+                        </div>
                     ))}
+                    </div>
                     <TheButton title="Закончить маршрут!" func={finishRoute} />
                 </div>
             </div>
