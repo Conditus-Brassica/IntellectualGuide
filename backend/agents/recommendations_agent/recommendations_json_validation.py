704b2fd8e90b9bf758e3cf3baec5317a852e6222
Vasilich563 <yackub563@mail.ru>
2024-01-27 18:24:44 +0300

    update json schema

--- backend/agents/recommendations_agent/recommendations_json_validation.py
+++ backend/agents/recommendations_agent/recommendations_json_validation.py
@@ -46,7 +46,9 @@ find_recommendations_for_coordinates_and_categories = \
             },
             "user_login": {"type": "string"},
             "amount_of_recommendations_for_point": {"type": "number"},
+            "amount_of_additional_recommendations_for_point": {"type": "number"},
             "maximum_amount_of_recommendations": {"type": "number"},
+            "maximum_amount_of_additional_recommendations": {"type": "number"},
             "optional_limit": {"type": ["number", "null"]},
         },
         "required": [
@@ -54,7 +56,9 @@ find_recommendations_for_coordinates_and_categories = \
             "categories_names",
             "user_login",
             "amount_of_recommendations_for_point",
-            "maximum_amount_of_recommendations"
+            "amount_of_additional_recommendations_for_point",
+            "maximum_amount_of_recommendations",
+            "maximum_amount_of_additional_recommendations"
         ],
         "additionalProperties": False
  }
